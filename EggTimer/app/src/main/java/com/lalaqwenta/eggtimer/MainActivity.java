c38746be413a16e5cf40d00991cb04fd46b54126
Memes <plmisha97@gmail.com>
2020-02-26 19:49:22 +0300

    EggTimer
    Ended TimerDemo app and created from beginning to the end EggTimer app.

---
+++ EggTimer/app/src/main/java/com/lalaqwenta/eggtimer/MainActivity.java
@@ -0,0 +1,104 @@
+package com.lalaqwenta.eggtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean isPushed = false;
+
+    SeekBar seekBar;
+
+    int oneSec = 1000;
+
+    TextView timeSetTextView,
+            timeLeftTextView;
+
+    CountDownTimer timer;
+
+    MediaPlayer mediaPlayer;
+
+    public void onClick (View view) {
+        Log.i("Info", "Egg was pushed.");
+        if (isPushed) {
+            isPushed = false;
+            timer.cancel();
+
+        }
+        else {
+            ImageButton imageButton = findViewById(R.id.imageButton);
+            imageButton.setImageResource(R.drawable.egg_without_crakc);
+            isPushed = true;
+            timer.start();
+        }
+    }
+
+    void changeTime(long timeLeft)
+    {
+        timeLeftTextView.setText(String.format("%d:%d", timeLeft/60, timeLeft%60));
+    }
+
+    void updateTimer(long progress) {
+        timer = new CountDownTimer((progress)*oneSec, oneSec)
+        {
+            public void onTick(long millisUntilFinished) {
+                Log.i("Info", String.format("%d left", millisUntilFinished/oneSec));
+                changeTime(millisUntilFinished/oneSec);
+            }
+            public void onFinish() {
+                mediaPlayer.start();
+                changeTime(0);
+                isPushed = false;
+                ImageButton imageButton = findViewById(R.id.imageButton);
+                imageButton.setImageResource(R.drawable.egg_with_crack);
+            }
+        };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBar);
+
+        timeLeftTextView = findViewById(R.id.timeLeftTextView);
+        timeSetTextView = findViewById(R.id.timeSetTextView);
+
+        final int progress = seekBar.getProgress();
+        timeSetTextView.setText(String.format("%d:%d", (progress+1)/60, (progress+1)%60));
+        changeTime(progress+1);
+        updateTimer(progress+1);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.beeps1);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                changeTime(progress + 1);
+                timeSetTextView.setText(String.format("%d:%d", (progress+1)/60, (progress+1)%60));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                timer.cancel();
+                isPushed = false;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                updateTimer(seekBar.getProgress()+1);
+            }
+        });
+
+    }
+}
--- TimerDemo/app/src/main/java/com/lalaqwenta/timerdemo/MainActivity.java
+++ TimerDemo/app/src/main/java/com/lalaqwenta/timerdemo/MainActivity.java
@@ -5,22 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Integer counter = 0;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = (TextView) findViewById(R.id.textView);
+
         final Handler handler = new Handler();
 
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                Log.i("Info", "A second passed by");
-
+                Log.i("Info", String.format("%d seconds have passed!", counter++));
+                textView.setText(String.format("%d seconds have passed!", counter));
                 handler.postDelayed(this, 1000);
+
             }
         };
 
